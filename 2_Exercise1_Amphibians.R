29351ed811f6b40834b4a446f15ab553fb400b24
Mel Torres <64219488+tulatorres@users.noreply.github.com>
2024-10-17 16:24:27 -0600

    Update 2_Exercise1_Amphibians.R

--- 2_Exercise1_Amphibians.R
+++ 2_Exercise1_Amphibians.R
@@ -39,7 +39,14 @@ install_and_load(c("sf",
                    "tmap", 
                    "ggspatial", 
                    "RColorBrewer",
-                   "landscapemetrics"))
+                   "landscapemetrics",
+                   "nlme",
+                   "spmoran",
+                   "graphics",
+                   "Hmisc",
+                   "corrplot",
+                   "PerformanceAnalytics",
+                   "spdep"))
 #_____________________________________________
 #### 1. Loading Spatial Data ####
 #_____________________________________________
@@ -134,11 +141,13 @@ df <- df %>%
 # ...or st_transform(crs = st_crs(wy))
 # epsg:26913 = NAD83, Zone 13
 
+
 # Now that we're in the same spatial reference system, let's plot using mapview!
 mapview(df, zcol = "Species", col.regions = brewer.pal(6, "Dark2")) +
   mapview(wy, zcol = "COUNTYNAME", col.regions = brewer.pal(23, "Spectral"), legend = F)
 
-# So, WY is the entire state of Wyoming, whereas we are interested in shrinking our study area to capture just the points. Let's create a study area shapefile 
+# cool!
+# So, our raster data is the entire state of Wyoming, whereas we are interested in shrinking our study area to capture just the points. Let's create a study area spatial file
 
 # Let's see what the min and max is for our sites:
 st_bbox(df)
@@ -149,8 +158,9 @@ study <- st_as_sfc(st_bbox(df))
 plot(study)
 plot(df$geometry, add = T)
 
-# Note that the bounding box is where the min/max points are. So, we'll increase study area by adding a 1000m buffer around the bounding box to include these points! 
-# this also helps with preventing "edge effects" when we work with raster data - we don't want the boundary of our study area to be the reason why our analyses gets messed up
+# Note that the bounding box is where the min/max points are. So, we'll increase the study area by adding a 1000m buffer around the bounding box
+# This buffer ensures we have our data within the study area
+# Creating a buffer also helps with preventing "edge effects" when we work with raster data - we don't want the boundary of our study area to be the reason why our analyses is biased
 
 buff <- st_buffer(study, dist = 1000)
 
@@ -169,35 +179,46 @@ asp <- terrain(r["dem"], v = "aspect", neighbors = 8, unit = "degrees")
 # creating a stack of the rasters that have same extent, origin, crs, resolution
 rs <- c(r["dem"], r["ffp"], r["gsp"], r["srr"], r["hli"], slope, asp)
 
-# we'll use the nlcd layer from the `r` raster stack in step 2c.
-
-#_____________________________________________
-#### 2. Analyzing Spatial Data ####
-#_____________________________________________
-##### 2a. Creating Pseudoabsences #####
-#_____________________________________________
-# While I have data for many species, if I analyze it as is, my analyses assumes (a) perfect detection and (b) I located all possible locations for amphibians in the study area. 
-# I am good at finding amphibians, but I'm not that good!
-# So, we will prepare our data with that in mind!
-# let's create pseudoabsences! 
-
-# to start, let's do one species for time's sake - boreal chorus frog (Pseudacris maculata, PSMA)
+# Let's focus on one species for time's sake - boreal chorus frog (Pseudacris maculata, PSMA)
 psma <- df %>% 
   filter(Species == "PSMA")
 
-names(psma)
+# let's also make sure we're getting one presence of PSMA per cell (this prevents over-inflating results and spatial autocorrelation)
+# one way I've found success is to rasterize the points, then vectorizing again.
+# creating a coordinates column 
+coords <- st_coordinates(psma)
 
-# let's create a random sample of points for PSMA within the study area
-rp <- st_sample(study, size = 2*nrow(psma), type = "random") %>% 
-  st_as_sf(data.frame(PointID = 1:nrow(psma), Present = 0)) %>% 
+# rasterizing points to a reference raster, nlcd:
+dat.r <- rasterize(cbind(coords[,1], coords[,2]), rc["nlcd"], value = 1, fun = mean)
+
+# converting rasters into points
+dat.rp <- as.points(dat.r)
+
+# cleaning up the psma file for further processing
+dat.psma <- st_as_sf(dat.rp) %>% 
+  mutate(PointID = 1:nrow(dat.rp),
+         Present = 1) %>% 
+  select(-mean)
+
+# we'll use the nlcd layer again from the `r` raster stack in step 2b.
+
+#_____________________________________________
+#### 2. Prepping Spatial Data for Analysis ####
+#_____________________________________________
+##### 2a. Creating Pseudoabsences #####
+#_____________________________________________
+# If I were to analyze the PSMA data as-is, the analyses assumes (a) perfect detection of PSMA and (b) I located all possible locations for PSMA in the study area. 
+# I am good at finding frogs, but I'm not that good!
+# So, we will prepare our data with that in mind!
+# let's create pseudoabsences! 
+
+rp <- st_sample(study, size = 2*nrow(dat.psma), type = "random") %>% 
+  st_as_sf(data.frame(PointID = 1:(2*nrow(dat.psma)), Present = 0)) %>% 
   rename(geometry = x)
 
 # let's add the random points to the presence points
-dat.psma <- psma %>% 
-  mutate(Present = 1) %>% 
-  rename(PointID = LabID) %>% 
-  select(PointID, Present, geometry) %>% 
-  rbind(rp)
+dat.psma <- dat.psma %>% 
+  rbind(rp) 
 
 # sometimes, R gets confused, so we're ensuring our dat.psma is still a spatial file
 st_geometry(dat.psma) <- "geometry"
@@ -220,20 +241,19 @@ summary(e)
 which(is.na(dat.psma), arr.ind = T)
 
 # combine covariates with our response variable (presence/absence)
-
 dat.psma <- dat.psma %>% 
   bind_cols(e) %>% 
-  mutate(Present = as.factor(Present)) %>% 
-  select(-ID)
-
+  mutate(Present = as.factor(Present),
+         AllID = 1:nrow(dat.psma)) %>% 
+  select(-ID) 
 
 #_____________________________________________
 ##### 2c. Calculating Landscape Metrics #####
 #_____________________________________________
-# Other predictor variables can be calculated using Landscape Metrics
-# Landscape Metrics describe the composition and configuration of different land uses within a region
+
+# Landscape Metrics calculate the composition and configuration of different land uses within a region
 # LMs are important for assessing habitat fragmentation/degradation, connectivity, and more
-# We'll manipulate the raster layer, National Land Cover Database (NLCD), to create separate raster layers important for amphibian presence.
+# We'll manipulate a raster layer, the National Land Cover Database (NLCD), to create separate raster layers important for amphibian presence.
 
 # First, let's create matrices for the different layers we want.
 
@@ -258,10 +278,10 @@ mowa <- matrix(c(0, 10, 0,
                  13, 99, 0), ncol = 3, byrow = T)
 
 # classifying using terra::classify
-frt <- classify(r["nlcd"], mfrt, right = NA)
-old <- classify(r["nlcd"], mold, right = NA)
-wet <- classify(r["nlcd"] mwet, right = NA)
-owa <- classify(r["nlcd"], mowa, right = NA)
+frt <- classify(rc["nlcd"], mfrt, right = NA)
+old <- classify(rc["nlcd"], mold, right = NA)
+wet <- classify(rc["nlcd"], mwet, right = NA)
+owa <- classify(rc["nlcd"], mowa, right = NA)
 
 names(frt)<- "forest"
 names(old)<- "open land"
@@ -272,15 +292,11 @@ rlm <- c(frt, old, wet, owa)  # creating a raster stack
 
 plot(rlm)
 
-# Let's crop the rlm objects to the study area
-rlm.cp <- crop(rlm, study)
-plot(rlm.cp)
-
-# now, we're not calculating data for the entire state! Let's look at these values compared to our sites
-
+# Let's look at these landuse values compared to our sites
 df.buff <- st_buffer(df, dist = 500, joinStyle = "ROUND")
 df.rlm <- terra::mask(x = rlm.cp, mask = df.buff, updatevalue = NA, touches = T)
 
+# we'll use tmap to observe these rasters
 tmap_mode("view")
 tm_shape(df.rlm) +
   tm_raster(n=2,
@@ -290,21 +306,22 @@ tm_shape(df.rlm) +
   tm_shape(df)+
   tm_dots(size = 0.2)
 
-
-# DEFINITIONS TO KNOW
-# landscape metrics function at three levels: patch, class, and landscape. 
-## patch = neighboring cells belonging to the same class, using the 8-neighbor rule 
-## class = summary of all patches belonging to one class. Cna be a distribution of patch-level metrics of all patches (mean), or consider patches of the same class for calculations
-## landscape = summarizes the whole landscape into one value
+# Before we calculate the landscape metrics, we need to know the level and class of landscape metric
+# LEVELS OF LANDSCAPE METRICS
+## Think of levels like spatial scale - how much of the landscape do we want in our analysis? Does our species interact on a small scale or large scale?
+## landscape metrics function at three levels: patch, class, and landscape. 
+### patch = neighboring cells belonging to the same class, using the 8-neighbor rule 
+### class = summary of all patches belonging to one class. Cna be a distribution of patch-level metrics of all patches (mean), or consider patches of the same class for calculations
+### landscape = summarizes the whole landscape into one value
 
 # CLASSES OF LANDSCAPE METRICS
-# There are six different landscape metrics:
-## Area and Edge Metrics: sizes of patches and classes and the amount of edge
-## Shape Metrics: Shape of patch, mainly by area and perimeter
-## Core Metrics: Area of patches that are not edge
-## Aggregation Metrics: How patches of the same class are clumped or isolated from each other (i.e., the spatial configuration of the landscape)
+## There are six different landscape metrics:
+### Area and Edge Metrics: sizes of patches and classes and the amount of edge
+### Shape Metrics: Shape of patch, mainly by area and perimeter
+### Core Metrics: Area of patches that are not edge
+### Aggregation Metrics: How patches of the same class are clumped or isolated from each other (i.e., the spatial configuration of the landscape)
 ## Diversity Metrics: Abundance and dominance/rareness of classes (landscape level only)
-## Complexity metrics: How close to entropy the landscape pattern is (landscape level only)
+### Complexity metrics: How close to entropy the landscape pattern is (landscape level only)
 
 # Let's take a look at our options:
 View(list_lsm())
@@ -320,27 +337,31 @@ View(list_lsm())
 # For this exercise, let's calculate some class-level metrics. 
 ## core area percentage of landscape (core area metric - lsm_c_cpland)
 ## patch density (aggregation metric - lsm_c_pd)
+## fractal dimension index (shape metric - lsm_c_frac_mn)
 ## aggregation (aggregation metric - lsm_c_ai) - for S&Gs
 
 
-# creating a list of the variables we're interested in:
-rlm.l <- list(rlm.cp["forest"], rlm.cp["wetland"], rlm.cp["open water"])
+# creating a list of the variables we're interested in.
+rlm.l <- list(rlm.cp["forest"], rlm.cp["open land"], rlm.cp["wetland"], rlm.cp["open water"])
 
 # running an lapply function for the landscape metrics we're interested in
 psma.lm <- lapply(seq_along(rlm.l), function(x)
-  sample_lsm(rlm.l[[x]], psma$geometry, level = "class", what = c("lsm_c_cpland", "lsm_c_pd", "lsm_c_ai"), shape = "square", size = 165, verbose = T, plot_id = psma$LabID))
-  
-# how is it calculating these landscape metrics? Via a *moving window*
-# MOVING WINDOW: 
-## A matrix that specifies the neighborhood and metric value for local neighborhood
+  sample_lsm(rlm.l[[x]], dat.psma$geometry, level = "class", what = c("lsm_c_cpland", "lsm_c_pd", "lsm_c_frac_mn", "lsm_c_ai"), shape = "square", size = 165, verbose = T, plot_id = dat.psma$AllID))
+
+# Let's see what this looks like
+View(psma.lm[[1]])
 
 
+# In this format, we can't do much. So, we'll pivot it to make more sense:
 # now, let's pivot the data so we can look at it
-
 psma.lm <- lapply(seq_along(psma.lm), function(x)
   pivot_wider(psma.lm[[x]], names_from = metric, values_from = value))
 
 View(psma.lm[[1]])
+
+
+# check to make sure the names were transferred correctly
+lapply(psma.lm, names)
 
 # note that there are some points that are duplicated - there are two class values per sample (1 and 0). What does this mean?
 
@@ -352,8 +373,6 @@ temp <- lapply(seq_along(psma.lm), function(x)
   filter(class == 1)
 )
 
-
-
 psma.lm <- lapply(seq_along(psma.lm), function(x)
   psma.lm[[x]] %>% 
   group_by(plot_id) %>% 
@@ -363,34 +382,37 @@ psma.lm <- lapply(seq_along(psma.lm), function(x)
   select(plot_id,
          cpland,
          pd,
+         frac_mn,
          ai) 
 )
 
+
 # rename the variables so we know which variable is associated with what landscape
-nms <- list("frt", "wet", "owa") # same order as the rlm vars
+nms <- list("frt","old", "wet", "owa") # same order as the rlm vars
 
 psma.lm <- lapply(seq_along(psma.lm), function(x)
   psma.lm[[x]] %>% 
     rename(setNames("cpland", paste("cpland", nms[[x]], sep = "_")),
            setNames("pd", paste("pd", nms[[x]], sep = "_")),
+           setNames("frac_mn", paste("fracc", nms[[x]], sep = "_")),
            setNames("ai", paste("ai", nms[[x]], sep = "_"))
-           )
+    )
 )
 
-# check to make sure the names were transferred correctly
-lapply(psma.lm, names)
 
-# now, let's join this into one table
+# now, let's join the LMs into one table
 
-psma.dat <- psma.lm[[1]] %>% 
+psma.lm.all <- psma.lm[[1]] %>% 
   right_join(psma.lm[[2]]) %>% 
-  right_join(psma.lm[[3]])
+  right_join(psma.lm[[3]]) %>% 
+  right_join(psma.lm[[4]])
 
-View(psma.dat)
+psma.lm.all <- psma.lm.all %>% 
+  rename(AllID = plot_id)
 
 # now, for S&Gs, let's plot the relationship between two aggregate variables - the aggregation of patches (ai) and patch density (pd)
 
-psma.dat %>% 
+psma.lm.all %>% 
   ggplot(aes(x = ai_frt, y = pd_frt)) +
   geom_point()+
   geom_smooth(method = "lm") 
@@ -401,5 +423,146 @@ summary(lm(ai_frt~pd_frt, data = psma.dat))
 # note how correlated these two aggregation metrics are?
 # Be careful when choosing landscape metrics - be sure to pick a metric from each category and not multiple from the same one! 
 # These two do tell us slightly different things, but the base is the same
+
+# Let's join these LMs to the main database
+dat.psma <- dat.psma %>% 
+  right_join(psma.lm.all)
+
+# let's check for NAs
+which(is.na(dat.psma), arr.ind = T)
+
+# we have NAs in these rows/columns. Let's view these points and see what's up
+na.dat <- dat.psma %>% 
+  filter(if_any(everything(), is.na))
+
+View(na.dat)
+
+# what do these columns have in common?
+# We can fortunately replace these values with a 0
+dat.psma[is.na(dat.psma)] <- 0
+
+# reordering variables - will be easier to call our variables this way
+names(dat.psma)
+dat.psma <- dat.psma %>% 
+  relocate(AllID, .before = Present)
+
+# Now, let's do some analyses!
+
+#_____________________________________________
+##### 3. Analyzing Spatial Data #####
+#_____________________________________________
+#_____________________________________________
+##### 3a. Testing for Independence #####
+#_____________________________________________
+# We have the following:
+## presence/pseudoabsence points
+## covariates/predictor variables/independent variables***
+## First, let's check to see how correlated our variables are by running a correlation matrix
+
+cor(st_drop_geometry(dat.psma[,-c(1:3)]), use = "complete.obs")
+
+# Below are a few ways to plot a correlation matrix:
+corrplot((cor(st_drop_geometry(dat.psma[,-c(1:3)]), method = "pearson")))
+
+res <- rcorr(as.matrix(st_drop_geometry(dat.psma[,-c(1:3)])))
+
+chart.Correlation(st_drop_geometry(dat.psma[,-c(1:3)]), histogram=TRUE, pch=19)
+
+# which variables are highly correlated with each other?
+# Also, why do we care about variable independence?
+
+# In general, if the absolute value of the correlation coefficient between variables =  0.7, then the two variables cannot be in the same model. 
+# Pick one of the two variables, then move on.
+
+
+# we can also use the `multi.collinearity` function in spatialEco to detect highly correlated variables:
+
+# first, create a data frame of just the variables:
+m <- dat.psma[,4:ncol(dat.psma)] 
+m <- st_drop_geometry(m)
+
+
+# inputing the data frame into the multi-collinearity function
+mcl <- multi.collinear(m, perm = T, leave.out = T, n = 1000, p=0.05, na.rm = T)
+
+View(mcl)
+
+# testing for multi-collinearity is great because it assesses which variables are highly collinear with multiple variables at one time. The higher the frequency, the more times the variable was associated with another.
+
+# To filter out our variables, we can remove them in clumps (for lack of a better word). We see a grouping of frequency = 700+, so let's filter those variables out first
+
+m <- m[,-which(names(m) %in% mcl[mcl$frequency > 700,]$variables)]
+
+# running again
+mcl <- multi.collinear(m, perm = T, leave.out = T, n = 1000, p=0.05, na.rm = T)
+
+View(mcl)
+# Now, we have a grouping of frequency =300+
+m <- m[,-which(names(m) %in% mcl[mcl$frequency > 300,]$variables)]
+
+mcl <- multi.collinear(m, perm = T, leave.out = T, n = 1000, p=0.05, na.rm = T)
+
+# We're at frequency = 0 for all variables! Now, we can continue!
+
+#_____________________________________________
+##### 3b. Linear Regression Models #####
+#_____________________________________________
+# Now that we have tested for variable independence, let's run a couple of models!
+# We'll use a generalized linear model (GLM) because we're working with presence/absence data (binary). Linear models (LM) in R are not great at dealing with presence/absence data
+
+# We have several variables, so we can hypothesize which variables influence PSMA presence on the landscape. 
+names(dat.psma)
+
+# water availability model
+mod.wet <- glm(formula = Present ~ cpland_wet + pd_wet + ai_wet + cpland_owa + pd_owa + fracc_owa + ai_owa, data = dat.psma %>% st_drop_geometry(), family = binomial(link="logit"))
+
+# topography model
+mod.top <- glm(formula = Present ~ dem+ srr + slope + aspect + hli, data = dat.psma %>% st_drop_geometry(), family = binomial(link="logit"))
+
+# precipitation model
+mod.prcp <- glm(formula = Present ~ ffp + gsp + slope + aspect + hli, data = dat.psma %>% st_drop_geometry(), family = binomial(link="logit"))
+
+# null model
+mod.null <- glm(formula= Present ~ ., data = dat.psma %>% st_drop_geometry(), family = binomial(link="logit"))
+
+# create your own model! What do you think?
+
+
+# Now, let's get coefficients
+# more parsimonious = less complex = better for models
+# let's use AIC! Remember, lower is better
+
+summary(mod.wet)
+summary(mod.top)
+summary(mod.prcp)
+summary(mod.null) # this is going to be nuts, but this is a demo, right? :)
+
+# Let's do some model validation, specifically using Area under the Receiving Operator Curve (AUC and ROC)
+# This model validation method tests the sensitivity and specificity (true positive vs false positive rate) of our model 
+(wetAUC <- auc(dat.psma$Present, mod.wet$fitted.values))
+(topAUC <- auc(dat.psma$Present, mod.top$fitted.values))
+(prcpAUC <- auc(dat.psma$Present, mod.prcp$fitted.values))
+
+# our topographic model is the best one based on the AUC
+# let's get the ROC plot to visualize the AUC
+pred <- prediction(fitted(mod.top), dat.psma$Present)
+perf <- performance(pred, measure="tpr", x.measure="fpr")
+plot(perf, col=rainbow(10))
+abline(coef=c(0,1))
+
+# It's fine and dandy to create a model, but being able to predict back to the landscape is what we want. WHERE should we expect to have a good chance of PSMA occurring?
+pred<- predict(rs, mod.top, type='response', progress="text")
+
+ggplot() +
+  layer_spatial(pred) +
+  geom_sf(data = dat.psma, aes(group=as.factor(Present), color = as.factor(Present)))
+
+# Now we may want to choose some threshold: good for PSMA or not. 
+# We'll create a probabilities data frame for where PSMA are found, along with a data frame of inputs vs fitted values
+foundCactus<-data.frame(obs = dat.psma$Present, fitted = mod.top$fitted.values) %>% 
+  group_by(obs) %>% 
+  summarise(Min = min(fitted), 
+            Max = max(fitted))
+
 
 
